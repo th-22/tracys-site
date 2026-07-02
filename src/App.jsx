53d4395a8f4656744c91da0f6dd7f30d94e5ccdf
thomas lu <tlu2004@uw.edu>
2026-07-02 01:26:32 -0700

    added location and services

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Hero from "./sections/Hero";
 import PlaceholderSection from "./sections/Placeholder";
 import Navbar from "./components/Navbar"
 import About from "./sections/About";
+import Services from "./sections/Services";
+import Location from "./sections/Location";
 
 export default function App() {
 
@@ -10,7 +12,8 @@ export default function App() {
       <Navbar />
       <Hero />
       <About />
-      <PlaceholderSection id="services" label="SERVICES" bg="#9a8c98" />
+      <Services />
+      <Location />
       <PlaceholderSection id="gallery" label="GALLERY" bg="#4a4e69" />
       <PlaceholderSection id="contact" label="CONTACT" bg="#22223b" />
     </>
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,42 +1,50 @@
 import Section from "../components/Section";
+import haircutImage from '../assets/images/blow_dry.jpg';
 
 export default function Services() {
   return (
     <Section id="pricing">
       <div className="pricing-container">
 
-        <div className="pricing-header">
-          <h2>Pricing</h2>
-          <p>Simple, transparent rates based on time, detail, and hair length.</p>
-        </div>
-
-        <div className="pricing-grid">
-
-          <div className="price-card">
-            <h3>Men’s Haircut</h3>
-            <p className="price">$55+</p>
+    
+        <div className="pricing-info">
+          <div className="pricing-header">
+            <h2>Pricing</h2>
           </div>
+           <div className="pricing-grid">
 
-          <div className="price-card">
-            <h3>Women’s Haircut</h3>
-            <p className="price">$85+</p>
+            <div className="price-card">
+              <h3>Men’s Haircut</h3>
+              <p className="price">$55+</p>
+            </div>
+
+            <div className="price-card">
+              <h3>Women’s Haircut</h3>
+              <p className="price">$85+</p>
+            </div>
           </div>
+          <div className="pricing-grid">
+            <div className="pricing-note">
+              <p>
+                Every service is tailored. Pricing may vary slightly depending on hair length,
+                thickness, and the time needed to achieve your result.
+              </p>
 
+              <p>
+                I focus on clean shape, movement, and something that grows out well —
+                not just how it looks when you leave the chair.
+              </p>
+            </div>
+          </div>
         </div>
+         
 
-        <div className="pricing-note">
-          <p>
-            Every service is tailored. Pricing may vary slightly depending on hair length,
-            thickness, and the time needed to achieve your result.
-          </p>
-
-          <p>
-            I focus on clean shape, movement, and something that grows out well —
-            not just how it looks when you leave the chair.
-          </p>
+       
+        <div className="pricing-image">
+          <img src={haircutImage} alt="haircut" />
         </div>
-
       </div>
+     
 
     </Section>
   );
